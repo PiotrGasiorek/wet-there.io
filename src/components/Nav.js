75ae94a20afcf16c0e80617e3ff6a01576fc71dc
Piotr <piotrjgasiorek@gmail.com>
2021-11-05 18:35:37 +0100

    Created nav component to display apps logo

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import logo from '../logo.svg';
+
+function Nav() {
+  return (
+    <nav>
+        <img width='180px' src={logo} alt='wet-there.io app for searching weather wherever you are'/>
+    </nav>
+  );
+}
+
+export default Nav;
