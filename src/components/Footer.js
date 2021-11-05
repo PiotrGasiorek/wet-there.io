1ea2f33b90e7bd3d446c171a481a5901f75310e8
Piotr <piotrjgasiorek@gmail.com>
2021-11-05 18:44:01 +0100

    Created Footer and Main components

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+        <p>Created by Piotr Gasiorek</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Board from './Board';
+import Header from './Header';
+
+function Main() {
+    const [apiResponse, setApiResponse] = useState('');
+
+    const handleApiResponse = (response) => {
+      setApiResponse(response);
+    }
+    return (
+        <main>
+            <Header
+                handleApiResponse={handleApiResponse} 
+            />
+            <Board 
+                searchedLocation={apiResponse}
+            />
+        </main>
+    );
+  }
+  
+  export default Main;
