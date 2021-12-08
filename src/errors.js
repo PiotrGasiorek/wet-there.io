60bd5519225a9604126f25ca688aa333711c677b
Piotr <piotrjgasiorek@gmail.com>
2021-12-08 13:20:42 +0100

    Created list of error messages to handle edge cases and msgs from API

---
+++ src/errors.js
@@ -0,0 +1,18 @@
+export const listOfError = [
+    {
+        code: 2007,
+        message: 'Sorry but we have run out of free trial for this month'
+    },
+    {
+        code: 1006,
+        message: 'Ooops we could not find that location'
+    },
+    {
+        code: 2008,
+        message: 'Sorry our license for data has been blocked'
+    },
+    {
+        code: 9999,
+        message: 'Sorry we have some internal problem'
+    }
+]
