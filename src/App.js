8f7c78b7f359e16c06fed261ecf3a9047c22177b
Piotr <piotrjgasiorek@gmail.com>
2021-12-08 20:12:43 +0100

    Switched from css to scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import './App.scss';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Main from './components/Main';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Nav/>
       <Main/>
       <Footer/>
